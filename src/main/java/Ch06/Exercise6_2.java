b999f69c03c14bed1026318fd701c99902dccc8c
maejo-o <efforterjisulee@gmail.com>
2023-11-14 00:10:05 +0900

    Exercise6-2

---
+++ src/main/java/Ch06/Exercise6_2.java
@@ -0,0 +1,35 @@
+package Ch06;
+
+public class Exercise6_2 {
+    public static void main(String[] args) {
+        Student s = new Student("홍길동", 1, 1, 100, 60, 76);
+        String str = s.info();
+        System.out.println(str);
+    }
+}
+
+class Student {
+    String sname;
+    int nn1, nn2, nn3, nn4, nn5;
+
+
+    Student(String name, int n1, int n2, int n3, int n4, int n5) {
+        super();
+        sname = name;
+        nn1 = n1;
+        nn2 = n2;
+        nn3 = n3;
+        nn4 = n4;
+        nn5 = n5;
+    }
+    int getTotal() {
+        return nn3 + nn4 + nn5;
+    }
+    double getAverage() {
+        double result = (double)getTotal()/3;
+        return result;
+    }
+    String info()  {
+        return sname + ", " + nn1 + ", " + nn2 + ", " + nn3 + ", " + nn4 + ", " + nn5 + ", " + getTotal() + ", " + getAverage();
+    }
+}
