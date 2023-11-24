73175120383ef5ce226ea29450fb4c4ab11b39d8
maejo-o <efforterjisulee@gmail.com>
2023-11-24 16:04:28 +0900

    Ex7-2

---
+++ src/main/java/Ch07/Ex7_2.java
@@ -0,0 +1,16 @@
+package Ch07;
+class Parent { int x = 10; /* super.x */ }
+class Child extends Parent {
+    int x = 20; // this.x
+    void method() {
+        System.out.println("x = " + x); // 20 예상
+        System.out.println("this.x = " + this.x);
+        System.out.println("super.x = " + super.x);
+    }
+}
+public class Ex7_2 {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.method();
+    }
+}
